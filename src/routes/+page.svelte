09249a8ad5f01868e0d3d6f2f4014503829c59c4
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-12-26 02:08:02 +0900

    :lipstick: 주사위 눈 모양으로 구조 및 스타일 수정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,7 +21,36 @@
 	<div class="dice-container">
 		<h2>주사위</h2>
 		{#each dice as { id, value, isStored } (id)}
-			<button>{`주사위${id}: ${value}`}</button>
+			<button class="dice"
+				>{#if value === 1}
+					<div class="dot center"></div>
+				{:else if value === 2}
+					<div class="dot top-left"></div>
+					<div class="dot bottom-right"></div>
+				{:else if value === 3}
+					<div class="dot top-left"></div>
+					<div class="dot center"></div>
+					<div class="dot bottom-right"></div>
+				{:else if value === 4}
+					<div class="dot top-left"></div>
+					<div class="dot top-right"></div>
+					<div class="dot bottom-left"></div>
+					<div class="dot bottom-right"></div>
+				{:else if value === 5}
+					<div class="dot top-left"></div>
+					<div class="dot top-right"></div>
+					<div class="dot center"></div>
+					<div class="dot bottom-left"></div>
+					<div class="dot bottom-right"></div>
+				{:else if value === 6}
+					<div class="dot top-left"></div>
+					<div class="dot top-center"></div>
+					<div class="dot top-right"></div>
+					<div class="dot bottom-left"></div>
+					<div class="dot bottom-center"></div>
+					<div class="dot bottom-right"></div>
+				{/if}</button
+			>
 		{/each}
 	</div>
 
@@ -47,3 +76,67 @@
 		<p>결과1</p>
 	</div>
 </main>
+
+<style>
+	.dice-container {
+		display: flex;
+	}
+
+	.dice {
+		position: relative;
+		width: 60px;
+		height: 60px;
+		background-color: white;
+		border: 1px solid #ccc;
+		border-radius: 5px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 20px;
+		font-weight: bold;
+		margin: 0 10px;
+	}
+
+	.dot {
+		width: 10px;
+		height: 10px;
+		background-color: #000;
+		border-radius: 50%;
+		position: absolute;
+	}
+
+	.top-left {
+		top: 20%;
+		left: 20%;
+	}
+
+	.top-right {
+		top: 20%;
+		right: 20%;
+	}
+
+	.top-center {
+		top: 20%;
+		right: 40%;
+	}
+
+	.center {
+		top: 40%;
+		left: 40%;
+	}
+
+	.bottom-left {
+		bottom: 20%;
+		left: 20%;
+	}
+
+	.bottom-right {
+		bottom: 20%;
+		right: 20%;
+	}
+
+	.bottom-center {
+		bottom: 20%;
+		right: 40%;
+	}
+</style>
