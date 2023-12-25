052e4196d1adbcf83d7a27b5d82071fb8a148dd1
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-12-26 02:39:34 +0900

    :sparkles: 주사위 눈 컴포넌트 분리

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import DiceEyes from './DiceEyes.svelte';
+
 	const MAX_ROLLS = 3;
 
 	let dice = Array.from({ length: 5 }, (_, i) => ({ id: i + 1, value: 0, isStored: false }));
@@ -37,36 +39,9 @@
 	<div class="dice-container">
 		<h2>주사위</h2>
 		{#each dice as { id, value, isStored } (id)}
-			<button class={`dice ${isStored ? 'stored' : ''}`} on:click={() => toggleStoreDice(id)}
-				>{#if value === 1}
-					<div class="dot center"></div>
-				{:else if value === 2}
-					<div class="dot top-left"></div>
-					<div class="dot bottom-right"></div>
-				{:else if value === 3}
-					<div class="dot top-left"></div>
-					<div class="dot center"></div>
-					<div class="dot bottom-right"></div>
-				{:else if value === 4}
-					<div class="dot top-left"></div>
-					<div class="dot top-right"></div>
-					<div class="dot bottom-left"></div>
-					<div class="dot bottom-right"></div>
-				{:else if value === 5}
-					<div class="dot top-left"></div>
-					<div class="dot top-right"></div>
-					<div class="dot center"></div>
-					<div class="dot bottom-left"></div>
-					<div class="dot bottom-right"></div>
-				{:else if value === 6}
-					<div class="dot top-left"></div>
-					<div class="dot top-center"></div>
-					<div class="dot top-right"></div>
-					<div class="dot bottom-left"></div>
-					<div class="dot bottom-center"></div>
-					<div class="dot bottom-right"></div>
-				{/if}</button
-			>
+			<button class={`dice ${isStored ? 'stored' : ''}`} on:click={() => toggleStoreDice(id)}>
+				<DiceEyes {value} />
+			</button>
 		{/each}
 	</div>
 
@@ -117,49 +92,6 @@
 
 	.stored {
 		border: 2px solid rgb(255, 110, 110);
-	}
-
-	.dot {
-		width: 10px;
-		height: 10px;
-		background-color: #000;
-		border-radius: 50%;
-		position: absolute;
-	}
-
-	.top-left {
-		top: 20%;
-		left: 20%;
-	}
-
-	.top-right {
-		top: 20%;
-		right: 20%;
-	}
-
-	.top-center {
-		top: 20%;
-		right: 40%;
-	}
-
-	.center {
-		top: 40%;
-		left: 40%;
-	}
-
-	.bottom-left {
-		bottom: 20%;
-		left: 20%;
-	}
-
-	.bottom-right {
-		bottom: 20%;
-		right: 20%;
-	}
-
-	.bottom-center {
-		bottom: 20%;
-		right: 40%;
 	}
 
 	.button {
---
+++ src/routes/DiceEyes.svelte
@@ -0,0 +1,77 @@
+<script>
+	export let value;
+</script>
+
+{#if value === 1}
+	<div class="dot center"></div>
+{:else if value === 2}
+	<div class="dot top-left"></div>
+	<div class="dot bottom-right"></div>
+{:else if value === 3}
+	<div class="dot top-left"></div>
+	<div class="dot center"></div>
+	<div class="dot bottom-right"></div>
+{:else if value === 4}
+	<div class="dot top-left"></div>
+	<div class="dot top-right"></div>
+	<div class="dot bottom-left"></div>
+	<div class="dot bottom-right"></div>
+{:else if value === 5}
+	<div class="dot top-left"></div>
+	<div class="dot top-right"></div>
+	<div class="dot center"></div>
+	<div class="dot bottom-left"></div>
+	<div class="dot bottom-right"></div>
+{:else if value === 6}
+	<div class="dot top-left"></div>
+	<div class="dot top-center"></div>
+	<div class="dot top-right"></div>
+	<div class="dot bottom-left"></div>
+	<div class="dot bottom-center"></div>
+	<div class="dot bottom-right"></div>
+{/if}
+
+<style>
+	.dot {
+		width: 10px;
+		height: 10px;
+		background-color: #000;
+		border-radius: 50%;
+		position: absolute;
+	}
+
+	.top-left {
+		top: 20%;
+		left: 20%;
+	}
+
+	.top-right {
+		top: 20%;
+		right: 20%;
+	}
+
+	.top-center {
+		top: 20%;
+		right: 40%;
+	}
+
+	.center {
+		top: 40%;
+		left: 40%;
+	}
+
+	.bottom-left {
+		bottom: 20%;
+		left: 20%;
+	}
+
+	.bottom-right {
+		bottom: 20%;
+		right: 20%;
+	}
+
+	.bottom-center {
+		bottom: 20%;
+		right: 40%;
+	}
+</style>
