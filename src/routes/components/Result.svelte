e40bdc888cfb9dae0a12dd34e1b15c875096d390
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-12-30 03:49:50 +0900

    :sparkles: Result.svelte 컴포넌트 추가

---
+++ src/routes/components/Result.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let result = {};
+
+	const resultCategories = [
+		'Ones',
+		'Twos',
+		'Threes',
+		'Fours',
+		'Fives',
+		'Sixes',
+		'Sub total',
+		'Bonus(35)',
+		'Choice',
+		'4 of a kind',
+		'Full house',
+		'Small straight',
+		'Large straight',
+		'Yacht',
+		'Total'
+	];
+</script>
+
+<table>
+	<thead>
+		<tr>
+			<th>항목</th>
+			<th>점수</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each resultCategories as category (category)}
+			<tr>
+				<td>{category}</td>
+				<td>0</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
