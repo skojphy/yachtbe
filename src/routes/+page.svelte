5b22d8e64788258af878e225882deffbd4979b44
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-12-30 05:19:49 +0900

    :fire: Info 영역 삭제

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import DiceEyes from './components/DiceEyes.svelte';
-	// import Info from './components/Info.svelte';
 	import Result from './components/Result.svelte';
 	import ToolTip from './components/ToolTip.svelte';
 	import { getRandomDiceValue } from '$utils';
@@ -102,17 +101,6 @@
 		{/if}
 		<button class="button reset-button" on:click={resetDice}>다시 굴리기</button>
 	</div>
-
-	<!-- <Info /> -->
-
-	<!-- <h2>주사위 보관소</h2>
-	<div class="stored-dice-container">
-		{#each storedDice as { id, value } (id)}
-			<div class="stored-dice">
-				<DiceEyes {value} isStored={true} />
-			</div>
-		{/each}
-	</div> -->
 
 	{#if showResult}
 		<div class="results">
